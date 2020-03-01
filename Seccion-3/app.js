8b534209a42a49091aed8d46983f49f64c826db5
Luis García <luis_grdz@hotmail.com>
2020-02-29 23:17:47 -0600

    Video 26: metodos

--- Seccion-3/app.js
+++ Seccion-3/app.js
@@ -2,5 +2,11 @@ var myApp = new Vue({
     el:"#app",
     data:{
         votos:0,
+    },
+    methods:{
+        votar:function(){
+            this.votos++;
+            console.log('Funcion votar con Vue');
+        }
     }
 });
