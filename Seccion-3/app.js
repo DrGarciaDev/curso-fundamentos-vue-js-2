075930652f692a6c8dc8a0e9dfb2c1371a65013d
Luis García <luis_grdz@hotmail.com>
2020-02-29 23:35:28 -0600

    Video 27: computed properties

--- Seccion-3/app.js
+++ Seccion-3/app.js
@@ -2,11 +2,18 @@ var myApp = new Vue({
     el:"#app",
     data:{
         votos:0,
+        nombre:"",
+        apellidos:""
     },
     methods:{
         votar:function(){
             this.votos++;
             console.log('Funcion votar con Vue');
+        }
+    },
+    computed:{
+        nombreCompleto:function(){
+            return this.nombre + " " + this.apellidos;
         }
     }
 });
