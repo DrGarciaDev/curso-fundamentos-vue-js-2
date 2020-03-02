c963cfd3bf6bea16af6faf77bd411724bd767528
Luis García <luis_grdz@hotmail.com>
2020-03-01 18:38:23 -0600

    Video 31: Usando metodos en un componente

--- Seccion-4/app.js
+++ Seccion-4/app.js
@@ -1,7 +1,13 @@
 Vue.component('mi-primer-componente',{
     // include props
     props:["msg"],
-    template: '<div class="alert alert-primary" role="alert">{{msg}}</div>'
+    template: '<div class="alert alert-primary" role="alert"><button class="btn btn-primary" v-on:click="saludar(msg)">Saludar</button> {{msg}}</div>',
+    methods:{
+        saludar:function(valor){
+            alert("Hola " + valor)
+        }
+    }
+
 });
 
 var app = new Vue({
