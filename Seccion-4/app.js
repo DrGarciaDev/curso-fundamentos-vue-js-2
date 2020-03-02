3fa15c5ccbdf577dc56400ae2656c5db065fba80
Luis García <luis_grdz@hotmail.com>
2020-03-01 18:25:24 -0600

    Video 29: primero componente sencillo

---
+++ Seccion-4/app.js
@@ -0,0 +1,10 @@
+Vue.component('mi-primer-componente',{
+    template: '<div class="alert alert-primary" role="alert">A simple primary alert—check it out!</div>'
+});
+
+var app = new Vue({
+    el:"#app",
+    data:{
+    
+    },
+});
