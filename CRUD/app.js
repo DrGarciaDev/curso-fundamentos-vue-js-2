0081be9830400bad5a0419fc281c8d3dae9798f3
Luis García <luis_grdz@hotmail.com>
2020-03-14 21:48:31 -0600

    Video 44: eliminar datos de firebase

--- CRUD/app.js
+++ CRUD/app.js
@@ -30,25 +30,34 @@ var app = new Vue({
                 titulo: titulo,
                 descripcion: descripcion
             }
-            app.lista.push(item);
+            // app.lista.push(item);
             firebase.database().ref('pasatiempos/'+clave).set(item);
+            app.recargarLista();
+        },
+        recargarLista: function(){
+            app.lista = [];
+            app.listarElementos();
         },
         eliminar:function(clave){
-            var index = app.lista.map(function(objeto){
-                return objeto.clave;
-            }).indexOf(clave);
+            // var index = app.lista.map(function(objeto){
+            //     return objeto.clave;
+            // }).indexOf(clave);
+            // app.lista.splice(index,1);
+            firebase.database().ref('pasatiempos/'+clave).remove();
+            app.recargarLista();
+        },
+        listarElementos:function(){
+            var datos = firebase.database().ref('pasatiempos');
 
-            app.lista.splice(index,1);
+            datos.on('value', function(snapshot){
+                snapshot.forEach(function(childSnapshot){
+                    var childData = childSnapshot.val();
+                    app.lista.push(childData);
+                });
+            });
         }
     },
     created: function(){
-        var datos = firebase.database().ref('pasatiempos');
-
-        datos.on('value', function(snapshot){
-            snapshot.forEach(function(childSnapshot){
-                var childData = childSnapshot.val();
-                app.lista.push(childData);
-            });
-        });
+        this.listarElementos();
     }
 });
